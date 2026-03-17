110ac7c183da7da2fd3153051c7fb0ca80c5d58f
howru556 <99855063+howru556@users.noreply.github.com>
2026-03-17 14:32:04 +1100

    Change creatorSecret to boolean value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const creatorSecret = "0"
+var creatorSecret = "false"
 
 function openImgLink() {
-    if (creatorSecret == "1") {
+    if (creatorSecret == "true") {
         window.open('https://i.etsystatic.com/46038560/r/il/df64f5/6500108215/il_570xN.6500108215_5zxo.jpg', '_blank').focus();
   } else {
         window.open('https://i1.sndcdn.com/avatars-000551092194-3vr5jy-t1080x1080.jpg', '_blank').focus();
@@ -9,7 +9,7 @@ function openImgLink() {
 }
 
 function changeCreatorSecret() {
-    creatorSecret = "1"
+    creatorSecret = "true"
 }
 
 clickbutton.onclick = function() { openImgLink(); };
