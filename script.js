c0b436beb5af6158c0878fcb201764a2b9855c6c
howru556 <99855063+howru556@users.noreply.github.com>
2026-03-17 14:27:42 +1100

    Implement image link opening based on creatorSecret
    Added functionality to open different image links based on the creatorSecret value and a function to change the creatorSecret.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function openImgLink() {
+const creatorSecret = "0"
 
+function openImgLink() {
+    if (creatorSecret == "1") {
+        window.open('https://i.etsystatic.com/46038560/r/il/df64f5/6500108215/il_570xN.6500108215_5zxo.jpg', '_blank').focus();
+  } else {
+        window.open('https://i1.sndcdn.com/avatars-000551092194-3vr5jy-t1080x1080.jpg', '_blank').focus();
+    }
 }
 
+function changeCreatorSecret() {
+    creatorSecret = "1"
+}
+
+clickbutton.onclick = function() { openImgLink(); };
+secretButton.onclick = function() { changeCreatorSecret(); };
